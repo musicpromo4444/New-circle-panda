63ed9e12c06ef9ebf4b39cca5e5752e62e54bc89
musicpromo4444 <52609980+musicpromo4444@users.noreply.github.com>
2026-09-20 13:19:13 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { defineConfig } from "vite";
+import { nitro } from "nitro/vite";
 import react from "@vitejs/plugin-react";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     tanstackStart({ server: { entry: "server" } }),
+    nitro(),
     react(),
     tailwindcss(),
     tsConfigPaths({ ignoreConfigErrors: true }),
